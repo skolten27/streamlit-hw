3c2858b36162317377fc2d0ba6e375a1aae7d51e
Kolten Smith <skolten27@gmail.com>
2024-11-19 13:04:07 -0700

    new main 1

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import zipfile
 import plotly.express as px
@@ -36,3 +35,26 @@ def ohw(df):
 
 data = load_name_data()
 ohw_data = ohw(data)
+
+st.title('My Cool Name App')
+
+with st.sidebar:
+    input_name = st.text_input('Enter a name:', 'Mary')
+    year_input = st.slider("Year", min_value=1880, max_value=2023, value=2000)
+    n_names = st.radio('Number of names per sex', [3, 4, 5, 6])
+
+
+tab1, tab2 = st.tabs(['Names', 'Year'])
+
+with tab1: 
+    name_data = data[data['name']==input_name].copy()
+    fig = px.line(name_data, x='year', y='count', color='sex')
+    st.plotly_chart(fig)
+
+with tab2:
+    fig2 = top_names_plot(data, year=year_input, n=n_names)
+    st.plotly_chart(fig2)
+
+    st.write('Unique Names Table')
+    output_table = unique_names_summary(data, 2000)
+    st.dataframe(output_table)
